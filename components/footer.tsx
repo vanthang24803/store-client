b1f68fede7977d7409190ace0c668019ad2639b2
vanthang24803 <nguyenthang24803@gmail.com>
2023-07-02 22:14:13 +0700

    Client Store 💀

---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t ">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sx text-black">
+          &copy; 2023 FakeStoreName , Inc, All right reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
